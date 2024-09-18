9cecc9fa6c2ba715a4059f9b836d1d5179de71ef
Kaikeferreirax <kaikeferreira106@gmail.com>
2024-09-18 01:16:39 -0300

    Projeto-clientes

---
+++ Atividade/PessoaJuridica.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Atividade
+{
+    public class PessoaJuridica : Clientes
+    {
+        public string cnpj { get; set; }
+        public string ie { get; set; }
+
+        public override void PagarImposto(float v)
+        {
+            this.valor = v;
+            this.valorImposto = this.valor * 20 / 100;
+            this.total = this.valor + this.valorImposto;
+        }
+
+    }
+}
